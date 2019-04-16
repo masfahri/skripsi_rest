7cb88483be5e1dc00a0c86fce3f9a49e64d6f826
unknown <hsevfakhri@gmail.com>
2019-04-17 02:39:07 +0700

    KATEGORI{CRUD} done

--- application/controllers/api/admin/kategori.php
+++ application/controllers/api/admin/kategori.php
@@ -21,6 +21,7 @@ class Kategori extends REST_Controller {
         $this->load->helper('jwt');
         $this->load->model('M_user');
         $this->load->model('M_vendor');
+        $this->load->model('Crud');
         
         date_default_timezone_set('Asia/Jakarta');
     }
@@ -50,6 +51,133 @@ class Kategori extends REST_Controller {
         return $this->response($validation, $code);
     }
 
+    public function index_get()
+    {
+        $token = $this->get('token');
+        $id = $this->get('id');
+
+        $config = [
+            [
+                'field' => 'token',
+                'label' => 'Token',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '%s Diperlukan',
+                ],
+            ],
+        ];
+
+        $data = $this->get();
+        $this->form_validation->set_data($data);
+        $this->form_validation->set_rules($config);
+        
+        if($this->form_validation->run()==FALSE){
+            $output = $this->form_validation->error_array();
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+        }else{
+            $validation['JWT'] = $this->_decrypt($token);
+            if ($validation['JWT']->status == 'admin') {
+                $data = array('id' => $id);
+                if (empty($id)) {
+                    $return['kategori_produk'] = $this->Crud->where('kategori_produk', $data);
+                }else{
+                    $return['kategori_produk'] = array($this->Crud->where('kategori_produk', $data));
+
+                }
+            }
+        }
+        return $this->set_response($return, REST_Controller::HTTP_OK); 
+    }
+
+    public function edit_post()
+    {
+        $token = $this->post('token');
+        $nama_kategori = $this->post('nama_kategori');
+        $id = $this->post('id');
+
+        $config = [
+            [
+                'field' => 'token',
+                'label' => 'Token',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '%s Diperlukan',
+                ],
+            ],
+        ];
+
+        $data = $this->post();
+        $this->form_validation->set_data($data);
+        $this->form_validation->set_rules($config);
+        
+        if($this->form_validation->run()==FALSE){
+            $output = $this->form_validation->error_array();
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+        }else{
+            $validation['JWT'] = $this->_decrypt($token);
+            if ($validation['JWT']->status == 'admin') {
+                $data = array(
+                    'nama_kategori' => $nama_kategori, 
+                );
+                $key = array('id' => $id);
+                $update = $this->Crud->update('kategori_produk', $key, $data);
+                if ($update) {
+                    $output['message'] = 'sukses update';
+                    $code = REST_Controller::HTTP_OK;
+                }else{
+                    $output['message'] = 'gagal update';
+                    $code = REST_Controller::HTTP_BAD_REQUEST;
+                }
+            }else{
+                return false;
+            }
+            $this->set_response($output, $code);
+        }
+    }
+
+    public function delete_post()
+    {
+        $token = $this->post('token');
+        $id = $this->post('id');
+
+        $config = [
+            [
+                'field' => 'token',
+                'label' => 'Token',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '%s Diperlukan',
+                ],
+            ],
+        ];
+        
+        $data = $this->post();
+        $this->form_validation->set_data($data);
+        $this->form_validation->set_rules($config);
+        
+        if($this->form_validation->run()==FALSE){
+            $output = $this->form_validation->error_array();
+            $this->set_response($output, REST_Controller::HTTP_BAD_REQUEST);
+        }else{
+            $validation['JWT'] = $this->_decrypt($token);
+            if ($validation['JWT']->status == 'admin') {
+                
+                $key = array('id' => $id);
+                $update = $this->M_vendor->delete('kategori_produk', $key);
+                if ($update) {
+                    $output['message'] = 'sukses delete';
+                    $code = REST_Controller::HTTP_OK;
+                }else{
+                    $output['message'] = 'gagal delete';
+                    $code = REST_Controller::HTTP_BAD_REQUEST;
+                }
+            }else{
+                return false;
+            }
+            $this->set_response($output, $code);
+        }
+    }
+
     public function _decrypt($token)
     {
         $CI =& get_instance();
@@ -59,7 +187,7 @@ class Kategori extends REST_Controller {
             'token' => $token
         );
         $validation = array();
-        $validation['JWT'] = JWT::validateTimestamp(Crypt::decrypt_($decrypt), 172800);
+        $validation['JWT'] = JWT::validateTimestamp(Crypt::decrypt_($decrypt), 1728000);
         return $validation['JWT'];
     }
 
---
+++ application/models/Crud.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+*
+*/
+class Crud extends CI_Model {
+
+    public function where($table, $key)
+    {
+        if (!empty($key['id'])) {
+            $this->db->where($key);
+        }
+        $query = $this->db->get($table);
+        if( $query->num_rows() > 0 ){
+            if( !empty($key['id']) ){
+                return $query->row_array();
+            }else{
+                return $query->result_array();  
+            }
+            
+        }
+    }
+
+    public function update($table, $key, $data)
+    {
+        $this->db->where($key);
+        return $this->db->update($table, $data);
+    }
+
+    public function delete($table, $key)
+    {
+        $this->db->where($key);
+        return $this->db->delete($table);
+    }
+
+}
+
+?>
